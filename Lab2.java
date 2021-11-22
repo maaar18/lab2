1dad5d7a2b98a13a0876a6da9cd25b5c10527b48
Marina <marovs18@mail.ru>
2021-11-22 17:31:43 +0300

    first commit

---
+++ Lab2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Lab2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите число:");
+        while (!sc.hasNextInt()) {
+            System.out.print("Ошибка, введите число снова:");
+            sc.next();
+        }
+        int n = sc.nextInt();
+        int m = 1;
+        System.out.print(n + "!=");
+        if (n < 15) {
+            while (n > 1){
+                m = m * n;
+                n--;
+            }
+            System.out.println(m);
+        }
+        else {
+            System.out.println("Ошибка");
+        }
+
+        sc.close();
+
+    }
+}
+
